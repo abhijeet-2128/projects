9581d163c9f0c9c334f2e7189dfab68dc5b42f3a
abhijeet-2128 <abhijeet.srivastava@appinventiv.com>
2023-07-04 13:47:33 +0530

    code push

---
+++ Calculator/calc.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.post('/', (req, res) => {
+  res.send('Hello World!')
+  
+});
+
+app.post('/calculate/',(req,res)=>{
+	
+	var operator = req.body.TYPE;
+	var num1 = req.body.a;
+	var num2 = req.body.b;
+	var result;
+
+	switch(operator){
+		case "add":
+		result = add(num1, num2);
+		break;
+
+    case "mod":
+		result = (num1%num2);
+		break;
+
+		case "subtract":
+		result = subtract(num1, num2);
+		break;
+
+    case "power":
+		result = (Math.pow(num1,num2));
+		break;
+
+		case "multiply":
+		result = (num1*num2);
+		break;
+
+    case "square-root":
+		result = Math.sqrt(num1);
+		break;
+
+		case "divide":
+		result = divide(num1, num2);
+		break;
+
+		default:
+		result = "Sorry, please enter a valid operator!"
+	}
+
+	function add(a, b){
+		return a + b
+	}
+
+	function subtract(a, b){
+		return a - b
+	}
+
+	function multiply(a, b){
+		return a * b
+	}
+
+	function divide(a, b){
+		return a / b
+	}
+    var r=result.toString()
+	res.send(r);
+})
+const port = 3000;
+
+
+
+app.listen(port, (error) => {
+  console.log(`Example app listening on port ${port}`);
+});
---
+++ Calculator/indexAdd.js
@@ -0,0 +1,6 @@
+app.get('/add/:num1/:num2', function (req, res) {
+    var num1 = parseInt(req.params.num1)
+    var num2 = parseInt(req.params.num2)
+    var quotient = (num1 / num2).toString()
+    res.send(quotient)
+  })
