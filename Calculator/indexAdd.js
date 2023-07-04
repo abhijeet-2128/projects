9ae94c5591c20525e58c1458c80ebcc69f2b50f9
abhijeet-2128 <abhijeet.srivastava@appinventiv.com>
2023-07-04 14:24:00 +0530

    new changes

--- Calculator/indexAdd.js
+++ Calculator/indexAdd.js
@@ -2,5 +2,8 @@ app.get('/add/:num1/:num2', function (req, res) {
     var num1 = parseInt(req.params.num1)
     var num2 = parseInt(req.params.num2)
     var quotient = (num1 / num2).toString()
+
+
+    
     res.send(quotient)
   })
