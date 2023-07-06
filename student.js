2de94b04c58f5ce048845aa4837696f3b31a7862
abhijeet-2128 <abhijeet.srivastava@appinventiv.com>
2023-07-06 06:24:35 +0530

    update commit by taking pull

---
+++ student.js
@@ -0,0 +1,8 @@
+// The given code is an object literal in JavaScript that defines an object named fullName with two properties: name and lastName. The console.log(fullName) statement logs the fullName object to the console.
+const fullName = {
+    // name : Abhijeet, // "Abhijeet" is the correct format to write 
+    name : "Abhijeet",
+    lastName : "Srivastava"
+}
+
+console.log(fullName)
