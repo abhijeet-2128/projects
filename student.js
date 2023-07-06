f88abf22c38ca3c43abc67b24addfa7dd1d9282f
abhijeet-2128 <abhijeet.srivastava@appinventiv.com>
2023-07-06 13:14:56 +0530

    new commit

--- student.js
+++ student.js
@@ -1,13 +1,10 @@
 // The given code is an object literal in JavaScript that defines an object named fullName with two properties: name and lastName. The console.log(fullName) statement logs the fullName object to the console.
 const fullName = {
     // name : Abhijeet, // "Abhijeet" is the correct format to write 
-    userName : "Abhijeet-2128",
- 
+    name : "Abhijeet",
+    lastName : "qSrivastava"
 }
 
 console.log(fullName)
 
-// Undo and Commit to New Branch
-// Make sure you are on the branch to which you have been committing. Use git log to check how many commits you want to roll back. Then undo the commits with git reset HEAD~Nwhere “N” is the number of commits you want to undo.
 
-//que - jaise hmne kuch commit kia kisi branch pr jbtk hm git push nhi krte h tbtk github pr kuch show nhi hoga na ki hoga?
